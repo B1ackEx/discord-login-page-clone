cc559d17f423b5636a19344bdf462eb6acbb629f
B1ackEx_ <binw1736@gmail.com>
2026-06-14 23:24:11 +0500

    UPDATED

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-
 from database import login_collection
 
 app = FastAPI()
@@ -19,14 +18,9 @@ async def login(
     username: str = Form(...),
     password: str = Form(...)
 ):
-    login_data = {
+    await login_collection.insert_one({
         "username": username,
         "password": password
-    }
+    })
 
-    result = await login_collection.insert_one(login_data)
-
-    return {
-        "success": True,
-        "id": str(result.inserted_id)
-    }
+    return {"success": True}
