51c1cfa90fe97095bd24dd2e34fecc1869913564
B1ackEx_ <binw1736@gmail.com>
2026-06-14 18:41:48 +0500

    Backend

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+from database import login_collection
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+async def home():
+    return FileResponse("static/index.html")
+
+
+@app.post("/login")
+async def login(
+    username: str = Form(...),
+    password: str = Form(...)
+):
+    login_data = {
+        "username": username,
+        "password": password
+    }
+
+    result = await login_collection.insert_one(login_data)
+
+    return {
+        "success": True,
+        "id": str(result.inserted_id)
+    }
