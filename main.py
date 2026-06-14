24f64d3222346bbca2ba5b7ad0ddbcb656fb2554
B1ackEx_ <binw1736@gmail.com>
2026-06-14 15:11:04 +0500

    Changing Folders

--- main.py
+++ main.py
@@ -1,36 +1,6 @@
-from fastapi import FastAPI, Form
-from fastapi.middleware.cors import CORSMiddleware
-from database import login_collection
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# allow frontend to talk to backend
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-@app.get("/")
-def home():
-    return {"message": "API running"}
-
-@app.post("/login")
-async def login(
-    username: str = Form(...),
-    password: str = Form(...)
-):
-
-    data = {
-        "username": username,
-        "password": password  # (demo only)
-    }
-
-    result = await login_collection.insert_one(data)
-
-    return {
-        "success": True,
-        "id": str(result.inserted_id)
-    }
+app.mount("/static", StaticFiles(directory="static"), name="static")
